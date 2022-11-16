79e7849ccb4e7df5ecf177cd63f516c6fc42d5bc
Gislaine Mello Casa <50672568+gimelloc@users.noreply.github.com>
2022-11-16 18:37:57 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  images: {
+    domains: ['files.stripe.com', "s3-alpha-sig.figma.com"],
+  },
+
+
+}
+
+module.exports = nextConfig
